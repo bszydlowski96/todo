99d685910173ebe32d3c0cf588b35b9163495b40
Szexu <b.szydlowski96@gmail.com>
2025-08-29 20:10:22 +0200

    feat: [CCS-7] Added prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-
-import './App.css'
-import Button from './components/Button/Button'
+import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-
   return (
     <>
-    <Button label={'Kliknij'}
-    onClick={()=> alert('Kliknięto')}
-    disabled={false}/>
+      <Button
+        label={'Kliknij'}
+        onClick={() => alert('Kliknięto')}
+        disabled={false}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,17 @@
-const Button = ({label, onClick, disabled}:{label:string,onClick:()=>void,disabled:boolean}) =>{
-    return (
-        <button onClick={onClick} disabled={disabled}>{label}</button>
-    )
-}
+const Button = ({
+  label,
+  onClick,
+  disabled,
+}: {
+  label: string;
+  onClick: () => void;
+  disabled: boolean;
+}) => {
+  return (
+    <button onClick={onClick} disabled={disabled}>
+      {label}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
