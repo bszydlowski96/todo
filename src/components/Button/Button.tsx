149d664ca6e24fb0f7ada0be7402e86d14a19231
Szexu <b.szydlowski96@gmail.com>
2025-08-29 20:04:44 +0200

    feat: [CCS-7] Added template for button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+const Button = ({label, onClick, disabled}:{label:string,onClick:()=>void,disabled:boolean}) =>{
+    return (
+        <button onClick={onClick} disabled={disabled}>{label}</button>
+    )
+}
+
+export default Button
